c40d65821b3f85700db7ca797ce659f233d9ffb8
Radha Singh <radha061btcsai20@igdtuw.ac.in>
2021-09-25 13:47:03 +0530

    snake game

--- snake_game/js/index.js
+++ snake_game/js/index.js
@@ -1,134 +1,124 @@
 // Game Constants & Variables
-
-let inputDir = {x: 0, y:0};
-const foodSound = new Audio('food.wav');
-const gameOverSound = new Audio('gameover.wav');
-const moveSound = new Audio('move.wav');
-const musicSound = new Audio('music.mp3'); 
-let speed = 5;
+let inputDir = {x: 0, y: 0}; 
+const foodSound = new Audio('music/food.wav');
+const gameOverSound = new Audio('music/gameover.wav');
+const moveSound = new Audio('music/move.wav');
+const musicSound = new Audio('music/music.mp3');
+let speed = 19;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
-    {x:13, y:15}
-]
-food = {x:6, y : 7};
+    {x: 13, y: 15}
+];
+
+food = {x: 6, y: 7};
+
 // Game Functions
-function main(ctime){
-    window.requestAnimationFrame(main);  
-    musicSound.play();
-    // console.log(ctime);
-    if((ctime - lastPaintTime)/1000< (1/speed)){
+function main(ctime) {
+    window.requestAnimationFrame(main);
+    // console.log(ctime)
+    if((ctime - lastPaintTime)/1000 < 1/speed){
         return;
     }
     lastPaintTime = ctime;
     gameEngine();
 }
 
-function isCollide(snake){
-    // If you pump into yourself
+function isCollide(snake) {
+    // If you bump into yourself 
     for (let i = 1; i < snakeArr.length; i++) {
-        if(snake[i].x === snake[0].x &&snake[i].y == snake[0].y){
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
             return true;
         }
     }
-    // If you pump into the wall
-        if(snake[0].x >= 18 ||snake[0].x<=0 || snake[0].y >= 18 ||snake[0].y<=0){
-            return true;
-        }
+    // If you bump into the wall
+    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+        return true;
+    }
+        
+    return false;
 }
 
 function gameEngine(){
-    // Part 1: Updating the snake array & food
+    // Part 1: Updating the snake array & Food
     if(isCollide(snakeArr)){
         gameOverSound.play();
         musicSound.pause();
-        inputDir = {x: 0, y:0};
-        alert("Game over. Press any key to play again!");
-        snakeArr = [{x:13, y:15}];
+        inputDir =  {x: 0, y: 0}; 
+        alert("Game Over. Press any key to play again!");
+        snakeArr = [{x: 13, y: 15}];
         musicSound.play();
-        score = 0;
-
+        score = 0; 
     }
 
     // If you have eaten the food, increment the score and regenerate the food
-    if(snakeArr[0].y == food.y && snakeArr[0].x == food.x){
+    if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
         foodSound.play();
-        score+=1;
+        score += 1;
         if(score>hiscoreval){
-            hiscoreval = score;
-            localStorage.setItem("hiscore", JSON.stringify(hiscore));
+            // hiscoreval = score;
+            // localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+            // hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
         }
-        scoreBox.innerHTML = "Score : "+score;
-        snakeArr.unshift({x:snakeArr[0].x+inputDir.x, y:snakeArr[0].y+inputDir.y });
+        scoreBox.innerHTML = "Score: " + score;
+        snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
         let a = 2;
         let b = 16;
-        food = {x: Math.round(a+(b-a)*Math.random()), y: Math.round(a+(b-a)*Math.random())};
+        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
     }
 
-    // Moving the snake 
-    for (let i = snakeArr.length - 2; i >= 0; i--) {
-        
+    // Moving the snake
+    for (let i = snakeArr.length - 2; i>=0; i--) { 
         snakeArr[i+1] = {...snakeArr[i]};
-        
     }
+
     snakeArr[0].x += inputDir.x;
     snakeArr[0].y += inputDir.y;
 
-    // Part 2: Display the snake and food
+    // Part 2: Display the snake and Food
     // Display the snake
     board.innerHTML = "";
     snakeArr.forEach((e, index)=>{
-       snakeElement = document.createElement('div');
-       snakeElement.style.gridRowStart =  e.y;
-       snakeElement.style.gridColumnStart =  e.x;
-       snakeElement.classList.add('snake');
-       if(index ==0){
-           snakeElement.classList.add('head');
-       }
-       else{
-           snakeElement.classList.add('snake');
-       }
-       board.appendChild(snakeElement);
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+
+        if(index === 0){
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement);
     });
     // Display the food
     foodElement = document.createElement('div');
     foodElement.style.gridRowStart = food.y;
     foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add('food');
+    foodElement.classList.add('food')
     board.appendChild(foodElement);
+
+
 }
-
-
-
-
-
-
-
-
-
-
 
 
 // Main logic starts here
-
+musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
-if(hiscore == null){
+if(hiscore === null){
     hiscoreval = 0;
-    localStorage.getItem("hiscore", JSON.stringify(hiscoreval));
+    localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
 }
 else{
     hiscoreval = JSON.parse(hiscore);
-    hiscoreBox.innerHTML = "High Score: "+hiscore;
+    // hiscoreBox.innerHTML = "HiScore: " + hiscore;
 }
 
-
-
-
 window.requestAnimationFrame(main);
-window.addEventListener('keydown',e=>{
-    inputDir = {x:0,y:1} // Start the game
+window.addEventListener('keydown', e =>{
+    inputDir = {x: 0, y: 1} // Start the game
     moveSound.play();
-    switch(e.key){
+    switch (e.key) {
         case "ArrowUp":
             console.log("ArrowUp");
             inputDir.x = 0;
@@ -152,7 +142,6 @@ window.addEventListener('keydown',e=>{
             inputDir.x = 1;
             inputDir.y = 0;
             break;
-
         default:
             break;
     }

b51eb02181bde669f416bdbd5390faa731610187
Radha Singh <radha061btcsai20@igdtuw.ac.in>
2021-10-19 11:43:47 +0530

    correction in a href

--- login.php
+++ login.php
@@ -3,10 +3,10 @@ session_start();
 $server = "localhost";
 $user = "root";
 $password = "";
-$db = "sugnup";
+$db = "signup";
 
-$con = mysqli_connect($server,$user,$password,$db);
-if($con)
+$conn = mysqli_connect($server,$user,$password,$db);
+if($conn)
 {
     ?>
     <script>
@@ -23,40 +23,75 @@ else{
 
 }
 
-if(isset($_POST['submit']))
-{
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+// if(isset($_POST['submit']))
+// {
+//     $email = $_POST['email'];
+//     $password = $_POST['password'];
 
-    $email_search = "select * from signup where email = '$email'";
-    $query = mysqli_query($con,$email_search);
+//     $email_search = "select * from signup where email = '$email'";
+//     $query = mysqli_query($con,$email_search);
 
-    $email_count = mysqli_num_rows($query);
+//     $email_count = mysqli_num_rows($query);
 
-    if($email_count){
-        $email_pass = mysqli_fetch_assoc($query);
-        $db_pass = $email_pass['password'];
+//     if($email_count){
 
-        $pass_decode = password_verify($password, $db_pass);
+        
+//         $email_pass = mysqli_fetch_assoc($query);
+//         $db_pass = $email_pass['password'];
 
-        if($pass_decode){
-            echo"Login Successful";
-            // header('location:music.html');
+//         $pass_decode = password_verify($password, $db_pass);
+
+//         if($pass_decode){
+//             echo"Login Successful";
+//             // header('location:music.html');
 
             ?>
-            <script>
-                location.replace("music.php");
-            </script>
-            <?php
-        }
-        else{
-            echo "Password Incorrect";
-        }
+//             <script>
+//                 location.replace("music.html");
+//             </script>
+//             <?php
+//         }
+//         else{
+//             echo "Password Incorrect";
+//         }
 
 
-    }
-    else{
-        echo "Invalid Email";
+//     }
+//     else{
+//         echo "Invalid Email";
+//     }
+// }
+
+function test_input($data)
+{
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"]== "POST") {
+      
+    $email = test_input($_POST["email"]);
+    $password = test_input($_POST["password"]);
+    $stmt = $conn->prepare("SELECT * FROM signup");
+    $stmt->execute();
+    $users = $stmt->fetchAll();
+      
+    foreach($users as $user) {
+          
+        if(($user['email'] == $email) && 
+            ($user['password'] == $password)) {
+                header("Location: http://localhost/Music-Jam-Music-Website-/music.html");
+                exit;
+                
+        }
+        else {
+            echo "<script language='javascript'>";
+            echo "alert('WRONG INFORMATION')";
+            echo "</script>";
+            die();
+        }
     }
 }
 
